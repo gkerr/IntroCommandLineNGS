9d651a5737dfd8aedd13887e4f6fd9b33fe67d54
hdinkel <6j5fnb2xrg@snkmail.com>
2013-09-20 17:01:02 +0200

    add combined logo

--- conf.py
+++ conf.py
@@ -196,7 +196,8 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = '_static/HUBLogo.png'
+latex_logo = '_static/logos_combined.png'
+#latex_logo = '_static/HUBLogo.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
